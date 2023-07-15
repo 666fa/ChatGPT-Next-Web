988fd9fdee82f8db03d331371f760c1880b9dfb2
666fa <135809227+666fa@users.noreply.github.com>
2023-07-15 13:48:02 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -263,7 +263,7 @@ const cn = {
     DefaultTopic: "新的聊天",
     BotHello:`New AI助手公益版目前只开放的模型：
     3.5 —— 3.5-turbo-16k
-    4.0 ——— 4.0-0613
+    4.0 —— 4.0-0613
     未开放3.5-turbo-16k-0613和4.0-32k
     您可以切换需要的模型，其它模型将陆续开放..
     如需帮助请联系群管理🐧[QQ群](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nhdCL8T-Hzbe9cOnN30GZkuvO6IloyDC&authKey=lGds%2FmHHkzYVLDfDaqKIjWfgGU1eXV9Kv9JFQTPgIszZg2gwpOnKQD8BjsNjB8Wu&noverify=0&group_code=874239338)
