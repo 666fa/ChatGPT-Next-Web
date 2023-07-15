f617f101f18e75df23402e9c09e41ca7857e0ba6
666fa <135809227+666fa@users.noreply.github.com>
2023-07-15 15:27:01 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -266,8 +266,8 @@ const cn = {
     4.0 —— 4.0-0613
     未开放3.5-turbo-16k-0613和4.0-32k
     您可以切换需要的模型，其它模型将陆续开放..
-    如需帮助请联系群管理🐧[QQ群](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nhdCL8T-Hzbe9cOnN30GZkuvO6IloyDC&authKey=lGds%2FmHHkzYVLDfDaqKIjWfgGU1eXV9Kv9JFQTPgIszZg2gwpOnKQD8BjsNjB8Wu&noverify=0&group_code=874239338)
-    \n请发送你的问题吧！🚀`,
+    如需帮助请联系群管理🐧[QQ群](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=nhdCL8T-Hzbe9cOnN30GZkuvO6IloyDC&authKey=lGds%2FmHHkzYVLDfDaqKIjWfgGU1eXV9Kv9JFQTPgIszZg2gwpOnKQD8BjsNjB8Wu&noverify=0&group_code=874239338)微信内需要浏览器打开
+    \n请发送你的问题吧！✅`,
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
